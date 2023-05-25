cde8e3c1ef4aa640d99f0fb7cdd00b8d0b84b684
Bart Wesselink <b.wesselink@recognize.nl>
2023-05-25 11:55:48 +0200

    RECGEN-21 - Update environment

--- cli/src/environments/settings/tst.ts
+++ cli/src/environments/settings/tst.ts
@@ -2,7 +2,7 @@ import {Environment, EnvironmentType} from '../environment'
 
 export default {
   type: EnvironmentType.TST,
-  registryLoginClientId: '4ab70484-cf94-4e51-bdcf-f72d41d58aea',
+  registryLoginClientId: '0e296de7-c1e5-4bd8-b138-081cee0c9bc8',
   registryLoginAuthority: 'https://login.microsoftonline.com/7a459d40-231f-4b13-b39c-f2c484b45125',
-  registryUrl: 'sc3000.recognize.nl',
+  registryUrl: 'sc3000.tst.recognize.hosting',
 } as Environment
