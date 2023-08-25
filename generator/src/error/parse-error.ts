f892e09aa135ce46372ec6d93c40fe3ed100f805
Bart Wesselink <b.wesselink@recognize.nl>
2023-08-25 09:50:59 +0200

    RECGEN-54 - PR feedback

--- generator/src/error/parse-error.ts
+++ generator/src/error/parse-error.ts
@@ -1,10 +1,19 @@
 export class ParseError extends Error {
-  constructor(public path: string, public line: number, public column: number, public cause: string, private template?: string) {
+  public path: string;
+  public line: number;
+  public column: number;
+  public cause: string;
+  public template?: string;
+
+  constructor({path, line, column, cause, template}: { path: string, line: number, column: number, cause: string, template?: string }) {
     super(template ?
       `Parse error in ${path} at line ${line}, column ${column} in template ${template}: ${cause}` :
       `Parse error in ${path} at line ${line}, column ${column}: ${cause}`)
 
+    this.path = path
     this.line = line
     this.column = column
+    this.cause = cause
+    this.template = template
   }
 }
